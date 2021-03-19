db4003073004dc16bfbf32fd6743966f65209ab8
Mike Davis <mdavis16@radford.edu>
2021-03-19 15:43:10 -0400

    Initial Project Code

---
+++ header.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style = "margin-bottom: 2%">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    <a class="navbar-brand" href="index.php">
+      <img src="images/logo.png" width="25" height="25" alt="logo">
+      Tech Takeout
+    </a>
+
+  <div class="collapse navbar-collapse" id="navbarToggler">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Eat</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Favorites</a>
+      </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
+    </ul>
+ </div>
+ </nav>
---
+++ registerForm.php
@@ -0,0 +1,30 @@
+ <div id = "registerForm" class = "container" style="width: 400px;  padding: 20px; background-color: white;" >
+    <h1>Sign up</h1>
+    <hr>
+    <form>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name"  placeholder="Enter name">
+      </div>
+      <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email"  placeholder="Enter email">
+      </div>
+        <div class="form-group">
+          <label for="zipcode">Zip code</label>
+          <input type="text" class="form-control" id="zipcode"  placeholder="Enter your zip code">
+        </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <label for="confirm-password">Confirm Password</label>
+        <input type="confirm-password" class="form-control" id="confirm-password" placeholder="Confirm Password">
+      </div>
+      <hr>
+      <div  style = "text-align:center;">
+        <button type="submit" class="btn btn-danger">Join Now!</button>
+      </div>
+    </form>
+</div>
